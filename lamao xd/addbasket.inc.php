a6e32718da82f03bad52b71dd5141b8cea45a619
brillin <45040324+brillin@users.noreply.github.com>
2018-12-04 14:30:22 +0100

    Add files via upload

---
+++ lamao xd/addbasket.inc.php
@@ -0,0 +1,15 @@
+<?php
+if (isset("basketvin")) {
+  $basket = array();
+switch ($basket) {
+  case 'red':
+  $basket[] = "Red Wine, ";
+    break;
+  case 'white':
+  $basket[] = "White Wine, ";
+    break;
+  case 'black':
+  $basket[] = "Black Wine, ";
+    break;
+}
+}
---
+++ lamao xd/bag.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Checkout</title>
+    <link rel="stylesheet" href="css/victor.css">
+  </head>
+  <body>
+
+    <div class="toptop"></div>
+    <div id="nav">
+      <div id="left"></div>
+      <div id="middle"></div>
+      <div id="right"></div>
+      <div id="middleleft"></div>
+      <div id="middlemiddle"></div>
+      <div id="middleright"></div>
+      <div id="bottomleft"><a style="text-decoration: none;" href="starttest.php"><img src="img/poutlogo.png" class="logologo" alt="logo"></a></div>
+      <div id="bottommiddle">
+    <ul style="color: #8EE4AF;" class="navul">
+      <li> <a href="women.php">WOMEN</a></li>
+      <li> <a href="kids.php">KIDS</a></li>
+      <li> <a href="herr.php">MEN</a></li>
+      <li> <a href="sale.php">SALE</a></li>
+    </ul>
+      </div>
+      <div id="bottomright"><div class="textright"><a href="account.php">Log-in / Sign-up</a></div>
+    </div>
+
+
+    </div>
+
+    <div class="checkout">
+<div class="payment">
+<div class="titlecheckout">
+  Order Summary
+</div>
+<div class="price">
+Sub Total
+</div>
+
+</div>
+<form class="" action="index.html" method="post">
+  <button class="paybtn" type="submit" name="checkout">Checkout</button>
+</form>
+
+
+
+<div class="itemsummary">
+  <img src="img/wine1.jpg" alt="wine">
+  <div class="itemsummarytext">
+  <strong>Wine</strong><br />
+  Type: Black<br />
+  Size: XXL
+</div>
+</div>
+
+
+  </body>
+</html>
