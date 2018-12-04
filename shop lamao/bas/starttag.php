89479c8c66f9c6e0811ef4675340544dad8ef061
brillin <45040324+brillin@users.noreply.github.com>
2018-12-04 14:19:54 +0100

    Add files via upload

---
+++ shop lamao/bas/starttag.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <link rel="stylesheet" href="css/victor.css">
+  <link rel="stylesheet" href="css/herr.css">
+  <link rel="stylesheet" href="css/women.css">
+  <link rel="stylesheet" href="css/base.css">
+  <title>David är bra</title>
+
+  <?php include'bas/starttag.php' ?>
